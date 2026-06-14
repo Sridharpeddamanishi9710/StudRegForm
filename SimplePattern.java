6f4800dad8991f3d9301f2101854e29d94862995
Sridhar Pedhamanishi <peddamanishisridhar@gmail.com>
2026-06-14 13:04:06 +0530

    Refactor SimpleStructure to SimplePattern with file input

--- SimplePattern.java
+++ SimplePattern.java
@@ -1,14 +1,17 @@
-public class SimpleStructure{
-    public static void main(String args[])
-    {
+import java.io.File;
+import java.util.Scanner;
+
+public class SimplePattern {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(new File("input.txt"));
         int rows = sc.nextInt();
-        for(int i=1;i<=rows;i++){
-            for(int j=1;j<=i;j++)
-            {
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+        sc.close();
     }
 }
